d26c0b551b9c3af3c5c5d5e9440acc474787836a
Mario <mariprr@amazon.com>
2018-07-10 14:57:17 +0000

    updated motd and  apache default to troubleshoot no candidate version error

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,8 +5,8 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 if node['platform_family'] == "rhel"
-	package = "http"
-elsif node ['platform_family'] == "debian"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
 	package = "apache2"
 end
 
@@ -19,5 +19,3 @@ service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
-
-include_recipe 'apache::websites'
